216687e3a5b152bc9b3a42b8f51d6fff0d9e0f7d
Muhammad Daniyal <mr.dannii98@gmail.com>
2023-04-17 23:45:02 +0500

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+    msg: {
+        type: String,
+        required: true
+    }
+}
+)
+</script>
+<template>
+    <h1>{{ getname("M Daniyal") }} </h1>
+    <h1>{{ email }}</h1>
+    <h1>{{ mob }}</h1>
+    <h1>{{ mob==50 }}</h1>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            email: 'admin@gmail.com',
+            mob:'202',
+        }
+    },
+    methods:{
+        getname(name){
+            return name;
+        }
+    }
+}
+</script>
